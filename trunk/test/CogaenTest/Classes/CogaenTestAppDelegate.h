d738cb68900075a9a227b41357396162144a822d
roman.divotkey <roman.divotkey@9f0c1cfd-aa1e-d247-2d40-0b94dbf3b368>
2010-05-05 13:12:03 +0000

    Initial import.

---
+++ trunk/test/CogaenTest/Classes/CogaenTestAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CogaenTestAppDelegate.h
+//  CogaenTest
+//
+//  Created by Roman Divotkey on 04.05.10.
+//  Copyright FH OÖ Studienbetriebs GmbH 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CogaenTestAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
